0e39686392a419bca1fb78867be9b69418203096
Jhansi267 <regujhansi@gmail.com>
2025-04-22 03:12:38 +0530

    login page

--- laundry_app/src/App.jsx
+++ laundry_app/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.scss';
-
-function App() {
+import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import LoginPage from './pages/Login'
+import Signup from './pages/Signup'
+const App = () => {
   return (
-    <div className="App">
-      <h1>Laundry App</h1>
+    <div>
+      {/* <LoginPage /> */}
+      <Router>
+        {/* <LoginPage /> */}
+        <Routes>
+
+        <Route path='/login'element={<LoginPage/>} />
+        <Route path='/signup' element={<Signup/>} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
